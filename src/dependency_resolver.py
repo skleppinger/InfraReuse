63345b7f068f414f60c505996bff9bf73e2190a7
skleppinger <s.kleppinger@gmail.com>
2024-10-26 20:04:49 -0400

    temp update

--- src/dependency_resolver.py
+++ src/dependency_resolver.py
@@ -1,47 +1,60 @@
 
 from typing import Any, Dict
+import inspect
+from src.custom_exceptions import DependencyInjectionError
 
 class Policy:
     """Base class for all resolution policies."""
     def __init__(self, 
                  available_objects: dict[str, Any],
                  subclass_ok: bool = True,
-                 **kwargs):
+                 **kwargs) -> None:
         self.available_objects = available_objects
         self.subclass_ok = subclass_ok
         self.kwargs = kwargs
 
-    def resolve(self, arg_name: str, arg_type: type):
+    def resolve(self, arg_name: str, arg_type: type) -> Any:
         pass
 
 class ResolveByType(Policy):
     """Resolve by type."""
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
 
-    def resolve(self, arg_name: str, arg_type: type):
+    def resolve(self, arg_name: str, arg_type: type) -> Any:
         for obj in self.available_objects.values():
-            if self.subclass_ok and issubclass(obj, arg_type):
-                return obj
             if isinstance(obj, arg_type):
                 return obj
-        raise KeyError(f"No object of type {arg_type} found")
+            if inspect.isclass(obj):
+                if self.subclass_ok and issubclass(obj, arg_type):
+                    return obj
+        return None
 
 class ResolveByName(Policy):
     """Resolve by name."""
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
 
-    def resolve(self, arg_name: str, arg_type: type):
-        return self.available_objects[arg_name]
+    def resolve(self, arg_name: str, arg_type: type) -> Any:
+        if arg_name in self.available_objects:
+            return self.available_objects[arg_name]
+        return None
 
 class ResolveByNameAndType(Policy):
     """Resolve by name and type."""
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
 
-    def resolve(self, arg_name: str, arg_type: type):
-        return self.available_objects[arg_name]
+    def resolve(self, arg_name: str, arg_type: type) -> Any:
+        if arg_name in self.available_objects:
+            if isinstance(self.available_objects[arg_name], arg_type):
+                return self.available_objects[arg_name]
+
+            if self.subclass_ok and issubclass(self.available_objects[arg_name], arg_type):
+                # TODO fix this is brittle - breaks when self.available_objects[arg_name] isn't a class
+                return self.available_objects[arg_name]
+
+        return None
 
 
 class DependencyResolver:
@@ -51,21 +64,40 @@ class DependencyResolver:
     def resolve(self):
         pass
 
-    def resolve_object_args(self, object_class: type, skip_args: tuple[str] = (), policy: Policy = ResolveByNameAndType, subclass_ok: bool = True):
+    def resolve_object_kwargs(self, object: type, skip_args: tuple[str] = (), policy: Policy = ResolveByNameAndType, subclass_ok: bool = True):
         """
         Resolve the arguments of an object.
 
         skip_args: tuple[str] - are the arguments that should not be resolved (usually the args that are passed in from the config).
-
+        policy
+        
         """
-        args = {}
-        for arg_name, arg_value in object_class.__init__.__code__.co_varnames:
-            if arg_name in skip_args:
-                args[arg_name] = arg_value
+        kwargs = {}
+        signature = inspect.signature(object)
+        to_resolve = []
+
+        for param in signature.parameters.values(): # How to get parent classes args/kwargs?
+            if param.name in skip_args:
+                continue
+            if param.kind in (param.VAR_KEYWORD, param.VAR_POSITIONAL):
+                continue
+
+            # check if policy can find the value
+            value = policy(subclass_ok=subclass_ok, available_objects=self._object_bag).resolve(arg_name=param.name, arg_type=param.annotation) 
+            if value is not None:
+                kwargs[param.name] = value
             else:
-                arg_type = object_class.__init__.__annotations__[arg_name]
-                args[arg_name] = policy(subclass_ok=subclass_ok, available_objects=self._object_bag).resolve(arg_name=arg_name, arg_type=arg_type)
-        return args
+                to_resolve.append(param)
+
+            # TODO in future, resolve union types and list of type x
+
+        for param in to_resolve:
+            if param.default != inspect._empty:
+                kwargs[param.name] = param.default
+            else:
+                raise DependencyInjectionError(f"No value provided for required argument {param.name}, unable to resolve {object.__name__}")
+        
+        return kwargs
 
     def add_object(self, object_instance: Any, name: str):
         self._object_bag[name] = object_instance
--- tests/test_dependency_resolver.py
+++ tests/test_dependency_resolver.py
@@ -1,6 +1,7 @@
 from src.dependency_resolver import DependencyResolver, ResolveByNameAndType, ResolveByName, ResolveByType
 from typing import Any  
 import pytest
+from src.custom_exceptions import DependencyInjectionError
 
 @pytest.fixture
 def resolver():
@@ -34,11 +35,61 @@ class ObjectD:
 
     def complete(self):
         return True
+    
+class ObjectSubclass(ObjectD):
+    def __init__(self, object_e_unique_param: str, a: ObjectA, float_arg: float, **kwargs: Any):
+        super().__init__(a, float_arg, **kwargs)
+        self.object_e_unique_param = object_e_unique_param
+
+    def complete(self):
+        return True
+    
+class ObjectWithDefaultArg:
+    def __init__(self, default_arg_var: str= "test", **kwargs: Any):
+        self.default_arg_var = default_arg_var
+
+    def complete(self):
+        return True
+    
+class ObjectWithNoArgs:
+    def __init__(self, **kwargs: Any):
+        pass
+
+    def complete(self):
+        return True
+    
+class ObjectWithNoTypehint:
+    def __init__(self, arg_without_typehint, **kwargs: Any):
+        self.arg_without_typehint = arg_without_typehint
+
+    def complete(self):
+        return True
+
+class ObjectWithUnionTypehint:
+    def __init__(self, arg_with_union_typehint: str | int, **kwargs: Any):
+        self.arg_with_union_typehint = arg_with_union_typehint
+
+    def complete(self):
+        return True
+    
+class ObjectWithOptionalTypehint:
+    def __init__(self, arg_with_optional_typehint: str | None, **kwargs: Any):
+        self.arg_with_optional_typehint = arg_with_optional_typehint
+
+    def complete(self):
+        return True
+    
+class ObjectWithListOfTypehint:
+    def __init__(self, arg_with_list_of_typehint: list[str], **kwargs: Any):
+        self.arg_with_list_of_typehint = arg_with_list_of_typehint
+
+    def complete(self):
+        return True
 
 def test_str_injection(resolver: DependencyResolver):
     str_arg = "test"
     resolver.add_object(str_arg, "str_arg")
-    args = resolver.resolve_object_args(ObjectB, policy=ResolveByNameAndType)
+    args = resolver.resolve_object_kwargs(ObjectB, policy=ResolveByNameAndType)
     b_init = ObjectB(**args)
     assert b_init.complete()
 
@@ -47,8 +98,90 @@ def test_str_injection_fails(resolver: DependencyResolver):
     resolver.add_object(str_arg, "wrong_name")
 
     # should match on name, but fail on type
-    args = resolver.resolve_object_args(ObjectC, policy=ResolveByNameAndType())
-    with pytest.raises(KeyError):
-        # args should be empty
-        ObjectB(**args)
+    with pytest.raises(DependencyInjectionError):
+        args = resolver.resolve_object_kwargs(ObjectC, policy=ResolveByNameAndType)
 
+    assert True
+
+def test_str_injection_fails_with_no_typehint(resolver: DependencyResolver):
+    str_arg = "test"
+    resolver.add_object(str_arg, "str_arg")
+
+    with pytest.raises(DependencyInjectionError):
+        args = resolver.resolve_object_kwargs(ObjectWithNoTypehint, policy=ResolveByNameAndType)
+
+def test_str_injection_fails_with_no_name(resolver: DependencyResolver):
+    str_arg = "test"
+    resolver.add_object(str_arg, "wrong_name")
+
+    with pytest.raises(DependencyInjectionError):
+        args = resolver.resolve_object_kwargs(ObjectWithNoTypehint, policy=ResolveByNameAndType)
+
+def test_int_injection(resolver: DependencyResolver):
+    int_arg = 1
+    resolver.add_object(int_arg, "int_arg")
+    args = resolver.resolve_object_kwargs(ObjectC, policy=ResolveByNameAndType)
+    c_init = ObjectC(**args)
+    assert c_init.complete()
+
+def test_module_injection(resolver: DependencyResolver):
+    object_b = ObjectB("test")
+    object_c = ObjectC(1)
+    object_a = ObjectA(object_b, object_c)
+    resolver.add_object(object_a, "a")
+    resolver.add_object(object_b, "ObjectB")
+    resolver.add_object(object_c, "ObjectC")
+    resolver.add_object(1.0, "float_arg")
+    args = resolver.resolve_object_kwargs(ObjectD, policy=ResolveByNameAndType)
+    d_init = ObjectD(**args)
+    assert d_init.complete()
+
+# TODO fix this to traverse MRO
+def test_subclass_injection(resolver: DependencyResolver):
+    object_e_unique_param = "test"
+    object_b = ObjectB("test")
+    object_c = ObjectC(1)
+    object_a = ObjectA(object_b, object_c)
+    resolver.add_object(object_a, "a")
+    resolver.add_object(1.0, "float_arg")
+    resolver.add_object(object_e_unique_param, "object_e_unique_param")
+    args = resolver.resolve_object_kwargs(ObjectSubclass, policy=ResolveByNameAndType)
+    subclass_init = ObjectSubclass(**args)
+    assert subclass_init.complete()
+
+def test_name_only_policy(resolver: DependencyResolver):
+    str_arg = "test"
+    resolver.add_object(str_arg, "wrong_name")
+    with pytest.raises(DependencyInjectionError):
+        args = resolver.resolve_object_kwargs(ObjectB, policy=ResolveByName)
+
+    # should match on name
+    resolver.add_object(str_arg, "str_arg")
+    args = resolver.resolve_object_kwargs(ObjectB, policy=ResolveByName)
+    b_init = ObjectB(**args)
+    assert b_init.complete()
+
+def test_type_only_policy(resolver: DependencyResolver):
+    string_arg = "1"
+    int_arg = 1
+    resolver.add_object(string_arg, "int_arg")
+    with pytest.raises(DependencyInjectionError):
+        args = resolver.resolve_object_kwargs(ObjectC, policy=ResolveByType)
+    
+    # should match on type, even with the wrong name
+    resolver.add_object(int_arg, "wrong_name")
+    args = resolver.resolve_object_kwargs(ObjectC, policy=ResolveByType)
+    c_init = ObjectC(**args)
+    assert c_init.complete()
+
+def test_optional_types(resolver: DependencyResolver):
+    str_arg = "test"
+    int_arg = 1
+    # resolver.add_object(str_arg, "str_arg")
+    resolver.add_object(int_arg, "int_arg")
+
+
+    args = resolver.resolve_object_kwargs(ObjectWithOptionalTypehint, policy=ResolveByNameAndType)
+    with_optional_typehint_init = ObjectWithOptionalTypehint(**args)
+    assert with_optional_typehint_init.complete()
+    assert with_optional_typehint_init.
